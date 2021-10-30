1f3fae91cb059996a050edadfc818893dab1e698
hgirish <girish@post.com>
2021-10-30 11:03:25 -0700

    FakeEventStore

---
+++ Chapter05/LoyaltyProgramUnitTests/FakeEventStore.cs
@@ -0,0 +1,33 @@
+﻿using SpecialOffers.EventFeed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyaltyProgramUnitTests
+{
+    public class FakeEventStore : IEventStore
+    {
+        public IEnumerable<EventFeedEvent> GetEvents(int start, int end)
+        {
+            if (start > 100)
+            {
+                
+                    Enumerable.Empty<EventFeedEvent>();
+            }
+            return Enumerable.Range(start, end - start)
+                .Select(i => new EventFeedEvent
+                (
+                    i,
+                    DateTimeOffset.UtcNow,
+                    "some event",
+                    new object()
+                ));
+        }
+
+        public void RaiseEvent(string name, object content)
+        {
+            
+        }
+    }
+}
